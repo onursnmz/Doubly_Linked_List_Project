cfc024d8c69f032ee9d76d65b0fe9c8d2fbb63d4
onur sönmez <onursnmz@outlook.com>
2021-12-12 22:03:41 +0300

    up
    up

---
+++ Onur_Sonmez/Form1.Designer.cs
@@ -0,0 +1,388 @@
+﻿namespace Onur_Sonmez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(60, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 214);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ÜRÜN EKLEME";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 172);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(127, 27);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 27);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(127, 27);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Fiyatı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adı";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kodu";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "EKLE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 249);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SİL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox2.Location = new System.Drawing.Point(406, 29);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(295, 214);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ÜRÜN SİLME";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(240, 46);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(49, 31);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Bul";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(104, 172);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(127, 27);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(104, 112);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(127, 27);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(104, 50);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(127, 27);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Fiyatı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 115);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 20);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Adı";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 50);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Kodu";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(872, 249);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 36);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "GÜNCELLE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.textBox7);
+            this.groupBox3.Controls.Add(this.textBox8);
+            this.groupBox3.Controls.Add(this.textBox9);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox3.Location = new System.Drawing.Point(747, 29);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(295, 214);
+            this.groupBox3.TabIndex = 6;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "ÜRÜN FİYAT GÜNCELLEME";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(240, 48);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(49, 31);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "Bul";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(104, 172);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(127, 27);
+            this.textBox7.TabIndex = 5;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(104, 112);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(127, 27);
+            this.textBox8.TabIndex = 4;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(104, 50);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(127, 27);
+            this.textBox9.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 20);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Fiyatı";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 115);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(33, 20);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Adı";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(16, 50);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(47, 20);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Kodu";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(406, 355);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(289, 212);
+            this.listBox1.TabIndex = 10;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(528, 314);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(49, 17);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "UYARI";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1082, 579);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ Onur_Sonmez/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Onur_Sonmez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class dugum
+        {
+            public string ad;
+            public int numara;
+            public int fiyat;
+            public dugum onceki;
+            public dugum sonraki;
+            
+        }
+        dugum ilk = null;
+        dugum temp = null;
+        dugum yeni = new dugum();
+        
+
+        public void print()
+        {
+            listBox1.Items.Clear();
+            dugum abc = new dugum();
+            abc = ilk;
+
+            while (abc != null)
+            {
+                listBox1.Items.Add(abc.numara + "---"+" "+ abc.ad+"---"+" "+ abc.fiyat.ToString()+" "+"TL");
+
+                abc = abc.sonraki;
+            }
+
+        }
+        
+        public void button1_Click(object sender, EventArgs e)
+        {
+            dugum yeni = new dugum();
+            yeni.ad = textBox2.Text;
+            yeni.numara = Convert.ToInt32(textBox1.Text);
+            yeni.fiyat = Convert.ToInt32(textBox3.Text);
+            if (ilk == null)
+            {
+
+                yeni.ad = textBox2.Text;
+                yeni.numara = Convert.ToInt32(textBox1.Text);
+                ilk = yeni;
+                ilk.sonraki = null;
+
+            }
+            else
+            {
+                dugum iter, temp;
+                iter = ilk;
+                if (yeni.numara < ilk.numara)
+                {
+                    temp = ilk;
+                    ilk = yeni;
+                    ilk.sonraki = temp;
+                }
+                else
+                {
+                    while (iter.sonraki != null && iter.sonraki.numara < yeni.numara)
+                    {
+                        iter = iter.sonraki;
+                    }
+                    yeni.sonraki = iter.sonraki;
+                    iter.sonraki = yeni;
+                }
+            }
+            print();
+            
+
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            dugum iter;
+            iter = ilk;
+            dugum temp;
+            if (ilk == null)
+            {
+                label10.ForeColor = Color.Red;
+                label10.Text = "LİSTEDE HERHANGİ BİR ELEMAN YOK!";
+            }
+            else
+            {
+                //Eğer sileceğimiz düğüm baştaysa, başlangıç düğümünü kaybetmemek adına temp adlı değişken
+                //oluşturup sileceğimiz düğümü o temp haline getirip öyle sildik. Daha sonrasında başlangıç düğümünü
+                //eski başlangıç'ın bir sonrasına attık(çünkü eski başlangıç silinmesi istendi)
+                if (ilk.numara == Convert.ToInt32(textBox6.Text))
+                {
+                    temp = ilk;
+                    ilk = ilk.sonraki;
+                    temp = null;
+                }
+                else
+                {
+                    while (iter.sonraki != null && iter.sonraki.numara != Convert.ToInt32(textBox6.Text))
+                    {
+                        iter = iter.sonraki;
+                    }
+                    if (iter.sonraki == null)
+                    {
+                        label10.ForeColor = Color.Red;
+                        label10.Text = "Girdiğiniz numara bulunamadı.";
+                    }
+                    else
+                    {
+                        temp = iter.sonraki;
+                        iter.sonraki = iter.sonraki.sonraki;
+                        temp = null;
+                    }
+                }
+            }
+            print();
+
+        }
+
+        public void button5_Click(object sender, EventArgs e)
+        {
+            temp = ilk;
+            dugum onceki;
+            dugum bul = new dugum();
+            bul.numara = Convert.ToInt32(textBox6.Text);
+            if (ilk.numara == bul.numara)
+            {
+                textBox5.Text = temp.ad;
+                textBox4.Text = Convert.ToString(temp.fiyat);
+            }else{
+
+                onceki = ilk;
+                temp = ilk.sonraki;
+
+
+
+
+                if (temp.numara == bul.numara)
+                {
+                    onceki.sonraki = temp.sonraki;
+                    textBox5.Text = temp.ad;
+                    textBox4.Text = Convert.ToString(temp.fiyat);
+                }
+
+
+                else
+                {
+                    label10.ForeColor = Color.Red;
+                    label10.Text = "Ürün bulunamadı.";
+                }
+
+            }
+
+        }
+            
+
+       public void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            temp = ilk;
+            dugum onceki;
+            dugum bul = new dugum();
+            bul.numara = Convert.ToInt32(textBox9.Text);
+            if (ilk.numara == bul.numara)
+            {
+                textBox8.Text = temp.ad;
+                textBox7.Text = Convert.ToString(temp.fiyat);
+            }
+            else
+            {
+
+                onceki = ilk;
+                temp = ilk.sonraki;
+
+
+
+
+                if (temp.numara == bul.numara)
+                {
+                    onceki.sonraki = temp.sonraki;
+                    textBox8.Text = temp.ad;
+                    textBox7.Text = Convert.ToString(temp.fiyat);
+                }
+
+
+                else
+                {
+                    label10.ForeColor = Color.Red;
+                    label10.Text = "Ürün bulunamadı.";
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dugum iter;
+            iter = ilk;
+            dugum temp;
+            if (ilk == null)
+            {
+                label10.ForeColor = Color.Red;
+                label10.Text = "LİSTEDE HERHANGİ BİR ELEMAN YOK!";
+            }
+            else
+            {
+                
+                while(iter.numara != Convert.ToInt32(textBox9.Text))
+                {
+                    iter = iter.sonraki;
+                }
+                    iter.ad = textBox8.Text;
+                    iter.fiyat = Convert.ToInt32(textBox7.Text);
+                
+                
+            }
+            print();
+        }
+    }
+    }
+
